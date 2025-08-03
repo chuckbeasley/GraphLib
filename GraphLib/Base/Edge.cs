46ca3e252cb162d1672779aa323a2b7616a8c67b
Chuck B. <chuck@chuckbeasley.com>
2025-08-02 22:41:30 -0400

    Refactor Edge class properties and constructor
    Updated Start and End properties to be non-nullable.
    Added a Description property with a default value.
    Modified the constructor to accept a description parameter.

--- GraphLib/Base/Edge.cs
+++ GraphLib/Base/Edge.cs
@@ -5,16 +5,18 @@
         where T2 : Vertex, new()
     {
         public int Weight { get; set; }
-        public T1? Start { get; set; }
-        public T2? End { get; set; }
+        public T1 Start { get; set; }
+        public T2 End { get; set; }
+        public string Description { get; set; } = string.Empty;
 
         private Edge() { }
 
-        internal Edge(T1 start, T2 end, int weight)
+        internal Edge(T1 start, T2 end, int weight, string description = null!)
         {
             Start = start;
             End = end;
             Weight = weight;
+            Description = description ?? string.Empty;
         }
     }
 }
